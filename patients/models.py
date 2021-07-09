7e49ea093757f0231d5ecc7fa165e990c9d9546c
Vladimir Mishakov <01.vladimir.mishakov@gmail.com>
2021-07-09 16:25:25 +0300

    Add firs db migration.

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    patronymic = models.CharField(max_length=60, null=True)
+    date_of_birth = models.DateField()
+    date_of_receipt = models.DateTimeField()
+    diagnosis = models.CharField(max_length=100, null=True)
+    appointment = models.TextField(max_length=10000, null=True)
+    comment = models.TextField(max_length=10000, null=True)
